1e911672d7a454ecb552b673723c9dc00c6a82d9
Alienchisel <69641074+Alienchisel@users.noreply.github.com>
2025-12-12 12:22:39 -0500

    Add simple history backup script

--- bash/backup_history.sh
+++ bash/backup_history.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 
+# Simple backup of shell history file
+
+HISTFILE="${HISTFILE:-$HOME/.zsh_history}"
+DEST="$HOME/history_backups"
+
+mkdir -p "$DEST"
+
+ts="$(date +'%Y-%m-%d_%H-%M-%S')"
+cp "$HISTFILE" "$DEST/history_$ts.txt"
+
+echo "History backed up to $DEST"
